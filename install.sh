b3ba63dd6d276182a02104768f1b42b71ec63682
Kim Ortvald <keiko@keikoware.dk>
2015-03-14 18:46:35 +0100

    added sudo to more commands

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 export DEBIAN_FRONTEND=noninteractive
-sudo "echo mysql-server-5.5 mysql-server/root_password password K31k0Ware | debconf-set-selections"
-sudo "echo mysql-server-5.5 mysql-server/root_password_again password K31k0Ware | debconf-set-selections"
+sudo echo mysql-server-5.5 mysql-server/root_password password K31k0Ware | sudo debconf-set-selections
+sudo echo mysql-server-5.5 mysql-server/root_password_again password K31k0Ware | sudo debconf-set-selections
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" apache2
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" php5 libapache2-mod-php5
