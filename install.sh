96e407796f1db02710c449c35b969d57632ffdc0
Kim Ortvald <keiko@keikoware.dk>
2015-03-15 00:04:37 +0100

    deafult page corrected

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ sudo apt-get install -q -y php5 libapache2-mod-php5
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server-5.5 mysql-client-5.5 php5-mysql
 
 mysql -u root -p Inf0Scr33n < ~/infoscreen/www/firmware/create_infoscreen_db.sql
-cp ~/infoscreen/www/* /var/www/ -r
+sudo cp ~/infoscreen/www/* /var/www/ -r
+sudo service apache2 restart
