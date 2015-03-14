ea33845674c3929533fbf1a657d08c98c137c5ca
Kim Ortvald <keiko@keikoware.dk>
2015-03-15 00:59:46 +0100

    mysql commandline updated

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get install -q -y apache2
 sudo apt-get install -q -y php5 libapache2-mod-php5
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server-5.5 mysql-client-5.5 php5-mysql
 
-mysql -u root -p Inf0Scr33n < ~/infoscreen/www/firmware/create_infoscreen_db.sql
+mysql --user=root --password=Inf0Scr33n < ~/infoscreen/www/firmware/create_infoscreen_db.sql
 sudo rm /var/www/* -r
 sudo cp ~/infoscreen/www/ /var -r
 sudo service apache2 restart
