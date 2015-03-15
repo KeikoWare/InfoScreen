4ed88e1e139260ef3b44f498e39879cd4d8b2c85
Kim Ortvald <keiko@keikoware.dk>
2015-03-15 02:13:45 +0100

    updated installer

--- install.sh
+++ install.sh
@@ -16,4 +16,4 @@ echo "@xset s off" > ~/.config/openbox/autostart
 echo "@xset -dpms" >> ~/.config/openbox/autostart
 echo "@xset s noblank" >> ~/.config/openbox/autostart
 echo "@chromium --kiosk --incognito http://localhost/infoscreen/onscreen" >> ~/.config/openbox/autostart
-
+sudo chmod +x ~/.config/openbox/autostart
