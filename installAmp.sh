b3e6f4c6a5b4c0cf6025b740463e9a48f7db05ab
Kim Ortvald <keiko@keikoware.dk>
2015-04-20 13:52:14 +0200

    installAmp.sh added

---
+++ installAmp.sh
@@ -0,0 +1,15 @@
+
+export DEBIAN_FRONTEND=noninteractive
+sudo echo mysql-server-5.5 mysql-server/root_password password Inf0Scr33n | sudo debconf-set-selections
+sudo echo mysql-server-5.5 mysql-server/root_password_again password Inf0Scr33n | sudo debconf-set-selections
+sudo apt-get update -q
+sudo apt-get install -q -y apache2
+sudo apt-get install -q -y php5 libapache2-mod-php5
+sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server-5.5 mysql-client-5.5 php5-mysql
+sudo apt-get install -q -y phpMyAdmin 
+
+# mysql --user=root --password=Inf0Scr33n < ~/infoscreen/www/firmware/create_infoscreen_db.sql
+# sudo rm /var/www/* -r
+# sudo cp ~/infoscreen/www/ /var -r
+
+sudo service apache2 restart
